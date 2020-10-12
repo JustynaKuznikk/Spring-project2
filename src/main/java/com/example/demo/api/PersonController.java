db9fda01351d83b888f64d877f8b5c85a408a12f
unknown <justyna.kuznikk@gmail.com>
2020-10-12 15:27:56 +0200

    properties

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,7 +3,10 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
     private final PersonService personService;
 
@@ -11,6 +14,7 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+    @PostMapping
     public void addPerson(Person person){
         personService.addPerson(person);
     }
