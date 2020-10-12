caa6b96e93dbfe2d697be004378b4a77093b86c4
unknown <justyna.kuznikk@gmail.com>
2020-10-12 15:55:05 +0200

    getPersonById method

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,9 +3,13 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -15,7 +19,17 @@ public class PersonController {
         this.personService = personService;
     }
     @PostMapping
-    public void addPerson(Person person){
+    public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
     }
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    public Person getPersonById(UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
+
+
 }
